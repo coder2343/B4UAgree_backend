e77c70155a0fd878e10c6d6879419e6c1493218f
Liam Walsh <liamspencerwalsh@icloud.com>
2024-04-29 21:11:38 -0500

    test change to avoid latency issues

--- hello.py
+++ hello.py
@@ -15,15 +15,17 @@ def get_privacy_policy():
   request_data = request.get_json()
   retrivedPolicy= request_data['privacyPolicy']
   PrivacyPolicy.append(retrivedPolicy)
+  print("append_privacyPolicy")
   return PrivacyPolicy
 
 # get request 
 @app.route('/sum', methods=['GET'])
 def send_summary():
-  # make_sum= get_summary(PrivacyPolicy.pop(),5)
-  make_sum = html_to_summary(PrivacyPolicy.pop())
+  print("request headers")
+  privacyPolicy= request.headers.get('privacyPolicy')
+  make_sum = html_to_summary(privacyPolicy)
+  print("return privacy policy sum")
   return json.dumps(make_sum)
-  # return json.dumps({"summary": make_sum})
 
 if __name__ == '__main__':
   app.run(port=5000)
