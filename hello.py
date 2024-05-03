801a8cb10130080529a67c26613c12304390e24b
Liam Walsh <liamspencerwalsh@icloud.com>
2024-05-03 09:31:34 -0500

    Revert "Changed API Endpoint Sum for Front End Fetch"
    This reverts commit 2eeee896c5933024daf00a583c89d760f85949bc.

--- hello.py
+++ hello.py
@@ -19,25 +19,15 @@ def get_privacy_policy():
   return PrivacyPolicy
 
 # get request 
-# LIAM'S REQUEST 05/02/2024
-# @app.route('/sum', methods=['GET'])
-# def send_summary():
-#   print("request headers")
-#   privacyPolicy= request.headers.get('privacyPolicy')
-#   make_sum = html_to_summary(privacyPolicy)
-#   print("return privacy policy sum")
-#   return json.dumps(make_sum)
 
 # get request 
-# needed to add a post for local server or it was rejected
-@app.route('/sum', methods=['POST','GET'])
+@app.route('/sum', methods=['GET'])
 def send_summary():
-  request_data = request.get_json()
-  retrivedPolicy= request_data['privacyPolicy']
-  make_sum = html_to_summary(retrivedPolicy)
+  print("request headers")
+  privacyPolicy= request.headers.get('privacyPolicy')
+  make_sum = html_to_summary(privacyPolicy)
+  print("return privacy policy sum")
   return json.dumps(make_sum)
-
-
 
 if __name__ == '__main__':
   app.run(port=5000)
